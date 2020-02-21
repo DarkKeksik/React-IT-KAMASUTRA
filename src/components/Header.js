af418b5c97c9fb84cc8d970f2b0471a3b62816f6
sychevskiy <frilans.er@mail.ru>
2020-02-21 18:04:26 +1000

    divided into components

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className={"header"}>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQJTfa8woRZ6dR1jeYlKtxXxX7NdN69s6xmaDCTssIm4ZPbhcOL"
+                alt=""
+                className="header__logo"
+            />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav
+            className={"nav"}
+        >
+            <div className={"nav__item"}>Profile</div>
+            <div className={"nav__item"}>Messages</div>
+        </nav>
+    )
+}
+
+export default Nav;
